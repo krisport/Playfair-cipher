a4edd83ce4d0d1010b7b5218d8f23ff588fb3a20
krisport <58785013+krisport@users.noreply.github.com>
2020-07-25 12:26:00 +0300

    Add some code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
+function stringToLow (key){
+	return key.toLowerCase();
+}
+function removeSpaces(key){
+	return key.trim();
+}
+function generateKeyTable(key){
+	let str, arr, alphabet, finalArr = [], num = 0;
+	key = stringToLow(key);
+	key =  removeSpaces(key);
+	alphabet = "abcdefghiklmnopqrstuvwxyz";
+	str = key + alphabet;
+	arr = str.split("").filter(arrayUnique);
+	finalArr[num] = [];
+	for (let i = 0; i < arr.length; i++) {
+		if(i%5==0 && i != 0){
+			num++;
+			finalArr[num] = [];
+		}
+		finalArr[num].push(arr[i]);
+	}
+	return finalArr;
+}
+function arrayUnique(value,index,self){
+	return self.indexOf(value) === index;
+}
+function encript(key,text){
+	
+}
 
+console.log(generateKeyTable("comedy"));
